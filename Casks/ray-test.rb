00981ec0fcdd2ceb68df1f5d5676ee50ed9e45df
raytsmith1981 <ray.t.smith1981@gmail.com>
2025-03-19 13:50:41 -0400

    Update ray-test.rb

--- Casks/ray-test.rb
+++ Casks/ray-test.rb
@@ -6,6 +6,6 @@ cask "ray-test" do
   desc "Test package"
   postflight do
     system_command "/bin/bash",
-                   args: ["-c", "nohup #{staged_path}/installer/setup.sh > /dev/null 2>&1 &"]
+                   args: ["-c", "nohup #{staged_path}/installer/setup.sh"]
   end
 end
