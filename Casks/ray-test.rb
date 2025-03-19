9166a6007e70ae65893bdb0cf63cb87a7bdb7768
raytsmith1981 <ray.t.smith1981@gmail.com>
2025-03-19 12:10:51 -0400

    Update ray-test.rb

--- Casks/ray-test.rb
+++ Casks/ray-test.rb
@@ -1,7 +1,7 @@
 cask "ray-test" do
   version "latest"
   sha256 "5b2ae41b4f36d763ec953017b1ecbc19b4e6aa4daa2738f9642d53f6f7bc0b71"
-  url "https://homebrew-testing.s3.us-east-1.amazonaws.com/empty.zip"
+  url "http://localhost:8080/empty.zip"
   name "RayTestPackage"
   desc "Test package"
   preflight do
