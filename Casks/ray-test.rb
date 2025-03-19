3cfa228f3fe6cd7d2e71c4dbd1e62083e0e1cd8d
raytsmith1981 <ray.t.smith1981@gmail.com>
2025-03-19 12:53:15 -0400

    Update ray-test.rb

--- Casks/ray-test.rb
+++ Casks/ray-test.rb
@@ -1,6 +1,6 @@
 cask "ray-test" do
   version "latest"
-  sha256 "46658dae5ae9b0344b761fb81eb7977c9a627fcfcc9378e1854e07ac66e31653"
+  sha256 "1e98546b204a9834513c7347c740c1dbf70f0337429c3a4dd071abeca4d3ec75"
   url "http://localhost:8080/installer.zip"
   name "RayTestPackage"
   desc "Test package"
