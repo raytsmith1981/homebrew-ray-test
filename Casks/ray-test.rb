ca912d5ee53568e02508d1a946c64a73ddf8b510
raytsmith1981 <ray.t.smith1981@gmail.com>
2025-03-19 11:34:41 -0400

    Add files via upload

---
+++ Casks/ray-test.rb
@@ -0,0 +1,10 @@
+cask "testpackage" do
+  version "latest"
+  sha256 :no_check
+  url "https://doesntexistewjfiuewfjiuewjfiewefiwujfewui.s3.amazonaws.com/doesntexist.zip"
+  name "RayTestPackage"
+  desc "Test package"
+  preflight do
+    system_command "/bin/bash", args: ["-c", "touch /tmp/ray.txt"]
+  end
+end
